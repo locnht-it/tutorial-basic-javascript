bca519dadc60ce884325e725c685ea327bf1274c
locnht-it <locnht.it@gmail.com>
2024-09-29 18:05:03 +0700

    create switch-case-statements lesson

---
+++ control-flow-basics/switch-case-statements/switch-case-statements.js
@@ -0,0 +1,50 @@
+console.log(`Switch-Case-Statements Lesson`);
+
+let day = ``;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+console.log(`Check day: `, day, new Date().getDay());
+
+let a = 0;
+switch (a) {
+  case 0:
+    day = "Sunday";
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+console.log(`Check day: `, day, a);
