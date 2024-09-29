3c4f08025522f7845f68c5af41c82da428005a8d
locnht-it <locnht.it@gmail.com>
2024-09-29 15:34:40 +0700

    create template-with-strings lesson

---
+++ javascript-basic/template-with-strings/template-with-strings.js
@@ -0,0 +1,8 @@
+console.log(`Template-with-Strings Lesson`);
+
+let a = 5;
+let b = 10;
+console.log(`a = ${a}, b = ${b}`);
+console.log(`Fifteen is ${a + b} and
+not ${2 * a + b}.`);
+console.log(`/product/${a}`);
