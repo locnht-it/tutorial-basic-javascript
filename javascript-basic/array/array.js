4983842e27c90c106110258ac3a4f17749efaa35
locnht-it <locnht.it@gmail.com>
2024-09-29 16:20:59 +0700

    create array lesson

---
+++ javascript-basic/array/array.js
@@ -0,0 +1,24 @@
+console.log(`Array Lesson`);
+
+// key: value
+let a = {
+  name: `LocNHT`,
+  age: `22`,
+};
+
+console.log(`a:`, a);
+console.log(`My name is: ${a.name}`);
+console.log(`My age is: ${a[`age`]}`);
+
+//arr = [item 1, item 2, item 3]
+let b = [`MU`, `Chelsea`, `Liver`];
+console.log(`Type of b : ${typeof b}`, b);
+console.log(`First item: ${b[0]},
+Second item: ${b[1]},
+Third item: ${b[2]}`);
+
+let friends = [
+  { name: "Loc Ngo", age: 22 },
+  { name: "Trung Bui", age: 22 },
+];
+console.log(`My friends: `, friends);
