4e572111ca2174ac50956b880c4bedaecb068ae7
locnht-it <locnht.it@gmail.com>
2024-09-29 13:28:15 +0700

    create variables-constants-comment lesson

---
+++ javascript-basic/variables-constants-comment/variables-constants-comment.js
@@ -0,0 +1,16 @@
+console.log(`Variables-Constants-Comment Lesson`);
+
+let a1 = 5;
+let a2 = 5.9;
+console.log(`a1 =`, a1, `; a2 =`, a2);
+
+let b1 = 5;
+let b2 = 5;
+let b3 = 6;
+console.log(`b1 =`, b1, `; b2 =`, b2, `; b3 =`, b3);
+console.log(`b1 = b2: `, b1 === b2);
+console.log(`b1 = b3: `, b1 === b3);
+
+const x = 5;
+// x = 6;
+console.log(`x =`, x);
