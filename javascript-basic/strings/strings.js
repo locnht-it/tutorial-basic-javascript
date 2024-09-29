191090b12004a14f3a2bde17bbd68ef18e0e9ee6
locnht-it <locnht.it@gmail.com>
2024-09-29 13:50:09 +0700

    create strings lesson

---
+++ javascript-basic/strings/strings.js
@@ -0,0 +1,11 @@
+console.log(`Strings Lesson`);
+
+console.log(4 + 5 + `a`); //9a
+console.log(`a` + 5 + 4); //a54
+
+let text1 = `We are the so-called "Vikings" from the north.`;
+console.log(`Text 1:`, text1);
+
+let text2 = `We are the so-called 
+"Vikings" from the north.`;
+console.log(`Text 2:`, text2);
